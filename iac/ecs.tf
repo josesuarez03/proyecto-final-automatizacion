4ea995a60652fa6027e9d27cdf3507d4c35c81b7
Jose Suarez <josysuarez2003@gmail.com>
2024-12-26 16:49:31 +0100

    create ecs

---
+++ iac/ecs.tf
@@ -0,0 +1,243 @@
+data "docker_registry_image" "api" {
+  name = "api:latest"
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "my-ecs-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecr_repository" "api" {
+  name = "api"
+}
+
+resource "docker_image" "api" {
+  name = "${aws_ecr_repository.api.repository_url}:latest"
+  build {
+    context = "./backend"
+  }
+}
+
+resource "docker_registry_image" "api_push" {
+  name = docker_image.api.name
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                = "app-task"
+  network_mode         = "bridge"
+  execution_role_arn   = aws_iam_role.ecs_execution_role.arn
+  task_role_arn        = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "api"
+      image = "${aws_ecr_repository.api.repository_url}:latest"
+      environment = [
+        { name = "DB_HOST", value = "db" },
+        { name = "DB_USER", value = "admin" },
+        { name = "DB_PASSWORD", value = "1234" },
+        { name = "DB_NAME", value = "syb" }
+      ]
+      dependsOn = [{ containerName = "db", condition = "START" }]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs_logs.name
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "api"
+        }
+      }
+    },
+    {
+      name  = "nginx"
+      image = "nginx:latest"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+      dependsOn = [{ containerName = "api", condition = "START" }]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs_logs.name
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "nginx"
+        }
+      }
+    },
+    {
+      name  = "db"
+      image = "mariadb:10.6"
+      environment = [
+        { name = "MARIADB_ROOT_PASSWORD", value = "root" },
+        { name = "MYSQL_PASSWORD", value = "1234" },
+        { name = "MYSQL_DATABASE", value = "syb" },
+        { name = "MYSQL_USER", value = "admin" }
+      ]
+      portMappings = [
+        {
+          containerPort = 3306
+          hostPort      = 3306
+        }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "db_data"
+          containerPath = "/var/lib/mysql"
+          readOnly      = false
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs_logs.name
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "db"
+        }
+      }
+    },
+    {
+      name  = "prometheus"
+      image = "prom/prometheus:latest"
+      portMappings = [
+        {
+          containerPort = 9090
+          hostPort      = 9090
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs_logs.name
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "prometheus"
+        }
+      }
+    },
+    {
+      name  = "grafana"
+      image = "grafana/grafana:latest"
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "grafana_data"
+          containerPath = "/var/lib/grafana"
+          readOnly      = false
+        }
+      ]
+      dependsOn = [{ containerName = "prometheus", condition = "START" }]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs_logs.name
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "grafana"
+        }
+      }
+    }
+  ])
+
+  volume {
+    name = "db_data"
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.db_data.id
+    }
+  }
+
+  volume {
+    name = "grafana_data"
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.grafana_data.id
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecs_instance_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs_instance_profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "my-ecs-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+}
+
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "/ecs/service"
+  retention_in_days = 30
+}
