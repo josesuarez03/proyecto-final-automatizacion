c63299bf51d0ac4038c89ff0228a167428dfa0e4
Jose Suarez <josysuarez2003@gmail.com>
2024-12-26 14:26:41 +0100

    iniciando la infraestructura con terraform

---
+++ iac/network.tf
@@ -0,0 +1,57 @@
+resource "aws_vpc" "main" {
+ cidr_block           = var.vpc_cidr
+ enable_dns_hostnames = true
+ tags = {
+   name = "main"
+ }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+ vpc_id = aws_vpc.main.id
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.internet_gateway.id
+ }
+}
+
+resource "aws_route_table_association" "subnet_route" {
+ subnet_id      = aws_subnet.subnet.id
+ route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_security_group" "security_group" {
+ name   = "ecs-security-group"
+ vpc_id = aws_vpc.main.id
+
+ ingress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = -1
+   self        = "false"
+   cidr_blocks = ["0.0.0.0/0"]
+   description = "any"
+ }
+
+ egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
