d92b3a3751878f08b8109919d2db1bfabf1675e6
Jose Suarez <josysuarez2003@gmail.com>
2025-01-03 11:09:45 +0100

    variables nuevas

--- iac/variables.tf
+++ iac/variables.tf
@@ -10,14 +10,25 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_cidr" {
-  description = "The CIDR block for the public subnet."
+variable "subnet_1_cidr" {
+  description = "CIDR block for subnet 1"
   type        = string
   default     = "10.0.1.0/24"
 }
 
+variable "subnet_2_cidr" {
+  description = "CIDR block for subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
 variable "allowed_ip" {
   description = "IP address allowed to access restricted services"
   type        = string
   default     = "81.41.129.51/32"
+}
+
+# Variables necesarias
+variable "environment" {
+  description = "Producción"
+  type        = string
 }
