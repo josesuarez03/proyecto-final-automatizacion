4c1498f436d363d53f1b06795d1555af45303a5f
Jose Suarez <josysuarez2003@gmail.com>
2025-01-03 11:09:52 +0100

    update

--- iac/network.tf
+++ iac/network.tf
@@ -1,17 +1,28 @@
 resource "aws_vpc" "main" {
- cidr_block           = var.vpc_cidr
- enable_dns_hostnames = true
- tags = {
-   name = "main"
- }
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    name = "main"
+  }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_cidr
+  cidr_block              = var.subnet_1_cidr
+  availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_2_cidr
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-2"
   }
 }
 
@@ -23,21 +34,27 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_route_table" "route_table" {
- vpc_id = aws_vpc.main.id
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.internet_gateway.id
- }
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
 }
 
-resource "aws_route_table_association" "subnet_route" {
- subnet_id      = aws_subnet.subnet.id
- route_table_id = aws_route_table.route_table.id
+resource "aws_route_table_association" "subnet_route_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.route_table.id
 }
+
+resource "aws_route_table_association" "subnet_route_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.route_table.id
+}
+
 
 resource "aws_security_group" "security_group" {
- name   = "ecs-security-group"
- vpc_id = aws_vpc.main.id
+  name   = "ecs-security-group"
+  vpc_id = aws_vpc.main.id
 
  ingress {
    from_port   = 22
