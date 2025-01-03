9d85376e6bf497f1032d902a58777d147a25ed09
Jose Suarez <josysuarez2003@gmail.com>
2025-01-03 16:46:35 +0100

    update

--- iac/ecs.tf
+++ iac/ecs.tf
@@ -11,7 +11,13 @@ resource "aws_ecs_service" "app_service" {
   name            = "app-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app_task.arn
-  desired_count   = 1
+  desired_count   = 2
+
+  force_new_deployment = true
+  
+  placement_constraints {
+    type = "distinctInstance"
+  }
   
   deployment_minimum_healthy_percent = 50
   deployment_maximum_percent = 200
