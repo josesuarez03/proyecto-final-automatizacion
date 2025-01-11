10307b0badded257fc1ea8654b1226937bab0de1
Jose Suarez <josysuarez2003@gmail.com>
2025-01-11 11:58:28 +0100

    correcion de la imagen y del balanceador de carga

--- iac/ec2.tf
+++ iac/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "ecs_lt" {
  name_prefix   = "ecs-template"
- image_id      = "ami-062c116e449466e7f"
+ image_id      = "ami-0e0568f9dc9d55f5d"
  instance_type = "t3.large"
 
  key_name               = "ec2ecsglog"
@@ -45,106 +45,119 @@ resource "aws_autoscaling_group" "ecs_asg" {
  }
 }
 
+# Application Load Balancer para tráfico HTTP
 resource "aws_lb" "ecs_alb" {
- name               = "ecs-alb"
- internal           = false
- load_balancer_type = "application"
- security_groups    = [aws_security_group.security_group.id]
- subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"  # Tipo application para HTTP
+  security_groups    = [aws_security_group.security_group.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 
- tags = {
-   Name = "ecs-alb"
- }
+  tags = {
+    Name = "ecs-alb"
+  }
+}
+
+# Network Load Balancer para MariaDB
+resource "aws_lb" "mariadb_nlb" {
+  name               = "mariadb-nlb"
+  internal           = false
+  load_balancer_type = "network"  # Tipo network para TCP
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+
+  tags = {
+    Name = "mariadb-nlb"
+  }
 }
 
 resource "aws_lb_listener" "http" {
- load_balancer_arn = aws_lb.ecs_alb.arn
- port              = 80
- protocol          = "HTTP"
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
- default_action {
-   type             = "forward"
-   target_group_arn = aws_lb_target_group.ecs_tg_80.arn
- }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg_80.arn
+  }
 }
 
 resource "aws_lb_listener" "grafana" {
- load_balancer_arn = aws_lb.ecs_alb.arn
- port              = 3000
- protocol          = "HTTP"
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 3000
+  protocol          = "HTTP"
 
- default_action {
-   type             = "forward"
-   target_group_arn = aws_lb_target_group.ecs_tg_3000.arn
- }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg_3000.arn
+  }
 }
 
 resource "aws_lb_listener" "kibana" {
- load_balancer_arn = aws_lb.ecs_alb.arn
- port              = 5601
- protocol          = "HTTP"
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 5601
+  protocol          = "HTTP"
 
- default_action {
-   type             = "forward"
-   target_group_arn = aws_lb_target_group.ecs_tg_5601.arn
- }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg_5601.arn
+  }
 }
 
 resource "aws_lb_listener" "mariadb" {
- load_balancer_arn = aws_lb.ecs_alb.arn
- port              = 3306
- protocol          = "TCP"
+  load_balancer_arn = aws_lb.mariadb_nlb.arn
+  port              = 3306
+  protocol          = "TCP"
 
- default_action {
-   type             = "forward"
-   target_group_arn = aws_lb_target_group.ecs_tg_3306.arn
- }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg_3306.arn
+  }
 }
 
 resource "aws_lb_target_group" "ecs_tg_80" {
- name        = "ecs-tg-80"
- port        = 80
- protocol    = "HTTP"
- target_type = "ip"
- vpc_id      = aws_vpc.main.id
+  name        = "ecs-tg-80"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
 
- health_check {
-   path = "/"
- }
+  health_check {
+    path = "/"
+  }
 }
 
 resource "aws_lb_target_group" "ecs_tg_3000" {
- name        = "ecs-tg-3000"
- port        = 3000
- protocol    = "HTTP"
- target_type = "ip"
- vpc_id      = aws_vpc.main.id
+  name        = "ecs-tg-3000"
+  port        = 3000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
 
- health_check {
-   path = "/api/health"
- }
+  health_check {
+    path = "/api/health"
+  }
 }
 
 resource "aws_lb_target_group" "ecs_tg_5601" {
- name        = "ecs-tg-5601"
- port        = 5601
- protocol    = "HTTP"
- target_type = "ip"
- vpc_id      = aws_vpc.main.id
+  name        = "ecs-tg-5601"
+  port        = 5601
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
 
- health_check {
-   path = "/api/status"
- }
+  health_check {
+    path = "/api/status"
+  }
 }
 
 resource "aws_lb_target_group" "ecs_tg_3306" {
- name        = "ecs-tg-3306"
- port        = 3306
- protocol    = "TCP"
- target_type = "ip"
- vpc_id      = aws_vpc.main.id
+  name        = "ecs-tg-3306"
+  port        = 3306
+  protocol    = "TCP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
 
- health_check {
-   protocol = "TCP"
- }
+  health_check {
+    protocol = "TCP"
+  }
 }
